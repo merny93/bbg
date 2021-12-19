aeca7234c4aa662ab6afd0c42affa491d6563cda
merny93 <simtart@gmail.com>
2021-12-19 15:21:58 -0500

    tada

--- integral_test.py
+++ integral_test.py
@@ -1,47 +1,91 @@
+from bbg import BLG_H
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import fsolve 
+from numpy.linalg import eigvalsh
 from functools import reduce
-N = 101
-A = 0.5/np.pi
-ks = np.linspace(-2,2, N)
-kxs,kys = np.meshgrid(ks,ks)
+from scipy.optimize import fsolve 
 
-e_func = kxs**2 + 0.5*kys**4
-mu = 1
-vs = plt.contour(ks,ks,e_func, levels=[mu]).collections[0].get_paths()[0].vertices
-# plt.show()
-# theta = np.linspace(0,2*np.pi, N)
-# vs = np.stack((np.cos(theta), np.sin(theta))).T
+N_k = 401
+a0=0.246
+Kmax = 0.025/a0*2
+kxs = np.linspace(-Kmax,Kmax,N_k) + 2*np.pi/(3*a0)
+kys = np.linspace(-Kmax,Kmax,N_k) + 2*np.pi/(3*np.sqrt(3)*a0)
 
+H = BLG_H([kxs, kys], D=1.0)
+evals = eigvalsh(H)
+
+ws = np.linspace(0.045, 0.055, 15)
+ws = list(ws) + [1,]
+plt.contourf(kxs,kys, evals[:,:,2], levels = ws, vmax = 0.055, vmin = 0.045, cmap = "hot")
+plt.colorbar()
+plt.gca().set_aspect(1)
+plt.xlabel("$k_x$ nm$^{-1}$")
+plt.ylabel("$k_y$ nm$^{-1}$")
+
+
+#now generate the contour for integrating
+vs = plt.contour(kxs,kys,evals[:,:,2], levels=[0.05], linewidths=0.0).collections[0].get_paths()[0].vertices
 lens = np.sqrt(np.sum(np.diff(vs, axis=0)**2, axis=1))
-# lens = 0.5*(lens + np.roll(lens,1))
 mid_points = 0.5*(vs[:-1,:] + vs[1:,:])
 mid_points = vs[:-1,:]
+plt.scatter(mid_points[::5,0], mid_points[::5,1],marker = "x", s=1)
+plt.savefig("sampled_for_integral.pdf")
+plt.clf()
 
+
+#define the V_kk'
 def V_kk(k,kk):
     return  (np.tensordot(k,kk, axes =(1,1)))**2
 
-# plt.clf()
-# plt.plot(V_kk(mid_points, mid_points)[:,0])
-# plt.show()
+#define the integral
 def rhs(delta):
     return np.sum(V_kk(mid_points, mid_points)*(delta*lens/np.sqrt(np.sum(mid_points**2,axis =1) + delta**2))[np.newaxis,:], axis = 1)
 
 
-# print(rhs(1*np.ones_like(lens)))
-# print(np.sum(lens))
-# exit()
-# res = fsolve(lambda x: x-rhs(x),10*np.ones_like(lens))
-def composite_function(*func):
-      
+def composite_function(*func):    
     def compose(f, g):
-        return lambda x : f(g(x))
-              
+        return lambda x : f(g(x))  
     return reduce(compose, func, lambda x : x)
+
+from mpl_toolkits.mplot3d import Axes3D
+
+
+res = 7500*np.ones_like(lens)
+
+steps = []
+for iter in range(5):
+    
+    steps.append(res)
+    res = rhs(res)
+
+
+from scipy.interpolate import interp1d as li
+
+ts = np.linspace(0,1, num = len(steps))
+samples = list(map(lambda x: li(ts, x), [[x[i] for x in steps] for i in range(res.size)]))
+t_fine =  np.linspace(0,0.3, num = 20)
+
+fig = plt.figure()
+plt.clf()
+plt.ion()
+for i,t in enumerate(t_fine):
+    z = [float(f(t)) for f in samples]
+    ax = fig.add_subplot(111, projection="3d")
+    ax.set_zlim(7000,8000)
+    ax.set_xlabel("$k_x$ nm$^{-1}$")
+    ax.set_ylabel("$k_y$ nm$^{-1}$")
+    ax.set_zlabel("$\Delta_k$")
+    ax.set_zticklabels([])
+    ax.plot(mid_points[:,0], mid_points[:,1], z)
+    fig.savefig(f"animation/frame_{i}.png")
+    fig.clf()
+
+
+
+    
 res = composite_function(*(10*(rhs,)))(-0.01*np.ones_like(lens))
 plt.clf()
 plt.plot(res)
 plt.show()
-# print(res - rhs(res))
+print(np.mean(res - rhs(res)))
 print(np.mean(res), np.std(res))
--- phase.py
+++ phase.py
@@ -8,17 +8,20 @@ from itertools import cycle
 
 if __name__ == "__main__":
     N_k = 201
-    N_d = 151
-    N_w = 151
+    N_d = 201
+    N_w = 201
+    # N_k = 101
+    # N_d = 101
+    # N_w = 101
     a0=0.246
     Kmax = 0.04/a0*2
     kxs = np.linspace(-Kmax,Kmax,N_k) + 2*np.pi/(3*a0)
     kys = np.linspace(-Kmax,Kmax,N_k) + 2*np.pi/(3*np.sqrt(3)*a0)
-    Ds = np.linspace(0, 1, N_d)
+    Ds = np.linspace(0.4, 0.8, N_d)
     Hs = np.array([BLG_H([kxs, kys], D=D) for D in Ds])
     evals = eigvalsh(Hs)
 
-    ws = np.linspace(0,0.08, N_w)
+    ws = np.linspace(0.02,0.04, N_w)
     # plt.contour(kxs,kys, evals[-1,:,:,2], levels=[ws[-1]])
     # plt.show()
 
@@ -27,6 +30,8 @@ if __name__ == "__main__":
         res = pool.starmap(DOS_fast ,job)
     doss = np.array(res)
     # doss = np.array([DOS_fast(ws, evals[i, :,:,2]) for i in range(N_d)])
-    plt.imshow(doss)
-    plt.show()
-    print(evals.shape)
+    plt.imshow(doss, extent=(ws[0], ws[-1], Ds[-1], Ds[0]), cmap="hot")
+    plt.gca().set_aspect("auto")
+    plt.xlabel("$e$V")
+    plt.ylabel("D (V/nm)")
+    plt.savefig("phase_diagram.pdf")
