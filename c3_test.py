695993eadef281c63a830501a9ad460d29aa3b7a
merny93 <simtart@gmail.com>
2021-12-22 11:06:00 -0500

    messing with parameters to find error

--- c3_test.py
+++ c3_test.py
@@ -1,9 +1,10 @@
 from bbg import BLG_H
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.linalg import eigvals as eigvalsh
+from numpy.linalg import eigvalsh
 from scipy import ndimage
-
+Params = dict({'g0':-2.61, 'g1':0.361 , 'g3':0.283, 'g4':0.138, 'Dp':0.015})
+Params = {'g0':-2.61, 'g1':0.361 , 'g3':0.283, 'g4':0.138, 'Dp':0.015}
 N_k = 401
 a0=0.246
 Kmax = 0.025/a0*2
@@ -12,9 +13,9 @@ kys = np.linspace(-Kmax,Kmax,N_k) + 2*np.pi/(3*np.sqrt(3)*a0)
 
 H = BLG_H([kxs, kys], D=1.0)
 evals = eigvalsh(H)
-print(np.mean(np.abs(np.imag(evals))))
-evals= np.real_if_close(evals)
-evals = np.sort(evals, axis =-1)
+# print(np.mean(np.abs(np.imag(evals))))
+# evals= np.real_if_close(evals)
+# evals = np.sort(evals, axis =-1)
 original = evals[:,:,2]
 plt.imshow(original)
 plt.show()
@@ -28,10 +29,10 @@ plt.contour(kxs,kys, rotated2, levels=[0.0457], colors="red")
 
 
 ##This line plots the original map with the countours overplotted
-plt.imshow(np.where(np.logical_and(rotated!=0, rotated2!=0), original, np.nan),vmax= 0.0457, origin="lower", extent=(kxs[0], kxs[-1], kys[0], kys[-1]))
+# plt.imshow(np.where(np.logical_and(rotated!=0, rotated2!=0), original, np.nan),vmax= 0.0457, origin="lower", extent=(kxs[0], kxs[-1], kys[0], kys[-1]))
 
 ##This line plots an error representation of the rotated masks
-# plt.imshow(np.where(np.logical_and(rotated!=0, rotated2!=0), np.abs(original-0.5*(rotated + rotated2)), np.nan), vmax = 4e-5, origin="lower", extent=(kxs[0], kxs[-1], kys[0], kys[-1]))
+plt.imshow(np.where(np.logical_and(rotated!=0, rotated2!=0), np.abs(original-0.5*(rotated + rotated2)), np.nan), vmax = 4e-5, origin="lower", extent=(kxs[0], kxs[-1], kys[0], kys[-1]))
 
 
 plt.colorbar()
